b8c2454135cc81b7c9f8a61f51cd47bb46476537
Code_Caster <53581330+Necr0Coder@users.noreply.github.com>
2022-07-22 07:55:08 -0400

    Added script and styles files

---
+++ script.js
@@ -0,0 +1,5 @@
+const container = document.querySelector(".container");
+const createDiv = document.createElement("div");
+createDiv.classList.add("squares");
+container.appendChild("createDiv");
+createDiv.textContent = "test";
