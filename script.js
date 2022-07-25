cbf5b8cea89a74841e0b25c69a4843a2b04a048a
necr0coder <jrhurley5333@gmail.com>
2022-07-25 19:25:58 -0400

    "init commit"

---
+++ script.js
@@ -0,0 +1,14 @@
+let container = document.getElementById("container");
+// container.appendChild(div);
+// div.textContent = "THIS IS A TEST";
+
+function createGrid(rows, cols) {
+  for (i = 0; i <= rows * cols; i++) {
+    let cells = document.createElement("div");
+    cells.classList.add(`cell`);
+    cells.innerText = "X";
+    container.appendChild(cells);
+  }
+}
+
+createGrid(16, 16);
