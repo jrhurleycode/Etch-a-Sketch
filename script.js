c8509ff07ac7487b6671d82b52059f61b98669c6
Code_Caster <53581330+Necr0Coder@users.noreply.github.com>
2022-07-26 07:50:38 -0400

    Changed operator from '<=' to just '<'.
    This removes the 1 additional in the grid

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let container = document.getElementById("container");
 // div.textContent = "THIS IS A TEST";
 
 function createGrid(rows, cols) {
-  for (i = 0; i <= rows * cols; i++) {
+  for (i = 0; i < rows * cols; i++) {
     let cells = document.createElement("div");
     cells.classList.add(`cell`);
     cells.innerText = "X";
