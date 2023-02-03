971ebec0e98f11f9dbfdd4efa567853e5bfbda93
Josh H <jrhurley5333@gmail.com>
2023-02-02 20:41:14 -0500

    testing push after username change

--- script.js
+++ script.js
@@ -19,80 +19,80 @@ document.body.onmouseup = () => (onMouseDown = false);
 
 //States
 colorButton.onclick = () => {
-  colorButton.className = "ACTIVATED";
-  eraser.className = "deactivated";
-  random.className = "deactivated";
+	colorButton.className = "ACTIVATED";
+	eraser.className = "deactivated";
+	random.className = "deactivated";
 };
 
 eraser.onclick = () => {
-  eraser.className = "ACTIVATED";
-  colorButton.className = "deactivated";
-  random.className = "deactivated";
+	eraser.className = "ACTIVATED";
+	colorButton.className = "deactivated";
+	random.className = "deactivated";
 };
 
 random.onclick = () => {
-  random.className = "ACTIVATED";
-  colorButton.className = "deactivated";
-  eraser.className = "deactivated";
+	random.className = "ACTIVATED";
+	colorButton.className = "deactivated";
+	eraser.className = "deactivated";
 };
 
 //Clear grid
 clear.onclick = () => {
-  createGrid(newGridSize);
+	createGrid(newGridSize);
 };
 
 //Slider
 slider.onmousemove = (e) => {
-  updateGridText(e.target.value);
-  createGrid(slider.value);
-  newGridSize = `${e.target.value}`;
+	updateGridText(e.target.value);
+	createGrid(slider.value);
+	newGridSize = `${e.target.value}`;
 };
 
 colorSelector.onclick = (e) => {
-  updateHexText(e.colorSelector.value);
+	updateHexText(e.colorSelector.value);
 };
 
 function updateHexText(value) {
-  colorSelector.innerHTML = `${value}`;
+	colorSelector.innerHTML = `${value}`;
 }
 
 function updateGridText(value) {
-  gridText.innerHTML = `${value} x ${value}`;
+	gridText.innerHTML = `${value} x ${value}`;
 }
 
 function updateColor(e) {
-  const red = Math.floor(Math.random() * 256);
-  const green = Math.floor(Math.random() * 256);
-  const blue = Math.floor(Math.random() * 256);
+	const red = Math.floor(Math.random() * 256);
+	const green = Math.floor(Math.random() * 256);
+	const blue = Math.floor(Math.random() * 256);
 
-  if (e.type == "mouseover" && !onMouseDown) return;
+	if (e.type == "mouseover" && !onMouseDown) return;
 
-  if (colorButton.className === "ACTIVATED") {
-    e.target.style.backgroundColor = `${colorSelector.value}`;
-  }
+	if (colorButton.className === "ACTIVATED") {
+		e.target.style.backgroundColor = `${colorSelector.value}`;
+	}
 
-  if (eraser.className === "ACTIVATED")
-    e.target.style.backgroundColor = "#ffffff";
+	if (eraser.className === "ACTIVATED") e.target.style.backgroundColor = "#ffffff";
 
-  if (random.className === "ACTIVATED")
-    e.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+	if (random.className === "ACTIVATED") e.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 }
 
 function createGrid(size) {
-  container.innerText = "";
-  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-  container.style.gap = `0px`;
+	container.innerText = "";
+	container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+	container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+	container.style.gap = `0px`;
 
-  for (i = 0; i < size * size; i++) {
-    let cells = document.createElement("div");
-    cells.setAttribute("id", `cell`);
-    cells.addEventListener("mousedown", updateColor);
-    cells.addEventListener("mouseover", updateColor);
-    container.appendChild(cells);
-  }
+	for (i = 0; i < size * size; i++) {
+		let cells = document.createElement("div");
+		cells.setAttribute("id", `cell`);
+		cells.addEventListener("mousedown", updateColor);
+		cells.addEventListener("mouseover", updateColor);
+		container.appendChild(cells);
+	}
 }
 
 createGrid(defaultGridSize);
 
 // Link to SO page on using variable between JS and CSS:  https://stackoverflow.com/questions/52563263/using-the-css-grid-repeat-declaration-in-javascript
+
+//test
